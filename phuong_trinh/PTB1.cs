f7c57f79f5ed4ebb13925acd3e20687ce8d40e3f
Ng Ngoc Hieu <hieudz12052005@gmail.com>
2025-11-11 01:19:42 +0700

    Add files via upload

---
+++ phuong_trinh/PTB1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phuong_trinh
+{
+    class PTB1
+    {
+        protected double a, b;
+
+        public PTB1(double a, double b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public virtual void Giai()
+        {
+            if (a == 0)
+            {
+                if (b == 0)
+                    Console.WriteLine("Phuong trinh vo so nghiem");
+                else
+                    Console.WriteLine("Phuong trinh vo nghiem");
+            }
+            else
+            {
+                double x = -b / a;
+                Console.WriteLine("Nghiem x = {0}", x);
+            }
+        }
+    }
+}
---
+++ phuong_trinh/PTB2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phuong_trinh
+{
+    class PTB2 : PTB1
+    {
+        private double c;
+
+        public PTB2(double a, double b, double c) : base(a, b)
+        {
+            this.c = c;
+        }
+
+        public override void Giai()
+        {
+            if (a == 0)
+            {
+                Console.WriteLine("Phuong trinh bac 1:");
+                base.Giai();
+                return;
+            }
+
+            double delta = b * b - 4 * a * c;
+            Console.WriteLine("Delta = {0}", delta);
+
+            if (delta < 0)
+            {
+                Console.WriteLine("Phuong trinh vo nghiem");
+            }
+            else if (delta == 0)
+            {
+                double x = -b / (2 * a);
+                Console.WriteLine("Nghiem kep x1 = x2 = {0}", x);
+            }
+            else
+            {
+                double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+                Console.WriteLine("x1 = {0}", x1);
+                Console.WriteLine("x2 = {0}", x2);
+            }
+        }
+    }
+}
